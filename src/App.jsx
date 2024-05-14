6588153dc1b66c5a0ea301cb312ab37b41b2eaa9
Mark Mesander <mark_mesander@hotmail.com>
2024-05-14 19:42:19 +0200

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-// Styles
-import './App.css'
+// Functions
+import {Route, Routes} from "react-router-dom";
 
 // Components
 import NavBar from "./components/NavBar.jsx";
+
+// Pages
+import Home from "./pages/home/Home.jsx";
+import Search from "./pages/search/Search.jsx";
+import Suggestion from "./pages/suggestion/Suggestion.jsx";
+import Lists from "./pages/lists/Lists.jsx";
+import Profile from "./pages/profile/Profile.jsx";
+
+// Styles
+import './App.css'
 
 function App() {
 
     return (
         <>
             <NavBar/>
-            <div>
-                <h1>Hello World</h1>
-            </div>
-            <div className="card">
-                <h2>Clean up</h2>
-            </div>
+            <Routes>
+                <Route exact path="/" element={<Home/>}/>
+                <Route path="/search" element={<Search/>}/>
+                <Route path="/suggestion" element={<Suggestion/>}/>
+                <Route path="/lists" element={<Lists/>}/>
+                <Route path="/profile" element={<Profile/>}/>
+            </Routes>
         </>
-    )
+    );
 }
 
 export default App
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,14 +10,55 @@ import "./NavBar.css";
 
 function NavBar() {
     return (
-        <nav className="nav-bar-outer-container">
-          <span className="logo-container">
-              <img src={logo} alt="Dodo & Kuik logo"/>
-              <h3>Dodo & Kuik</h3>
-          </span>
+        <nav className="navbar-outer-container">
+                <span className="logo-container">
+                    <img src={logo} alt="Dodo & Kuik app logo"/>
+                    <h3>Dodo & Kuik</h3>
+                </span>
+
+            <ul>
+                <li>
+                    <NavLink
+                        to="/"
+                        className={({isActive}) => isActive ? "active-nav-link" : "default-nav-link"}
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/search"
+                        className={({isActive}) => isActive ? "active-nav-link" : "default-nav-link"}
+                    >
+                        Search
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/suggestion"
+                        className={({isActive}) => isActive ? "active-nav-link" : "default-nav-link"}
+                    >
+                        Suggestion
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/lists"
+                        className={({isActive}) => isActive ? "active-nav-link" : "default-nav-link"}
+                    >
+                        Lists
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/profile"
+                        className={({isActive}) => isActive ? "active-nav-link" : "default-nav-link"}
+                    >
+                        Profile
+                    </NavLink>
+                </li>
+            </ul>
         </nav>
-
-
     );
 }
 

2ba83887bb9769fba26035bffba03c3b36283be5
Mark Mesander <mark_mesander@hotmail.com>
2024-05-14 18:07:26 +0200

    clean project

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
+// Functions
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// Pages
 import App from './App.jsx'
+
+// Styles
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(

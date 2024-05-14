99a9ea3b73c20d9501d712ed54e3afb5633c0704
Mark Mesander <mark_mesander@hotmail.com>
2024-05-15 00:38:43 +0200

    add loading and error section and styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,35 @@
 // Functions
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+// Components
+import Button from "../../components/button/Button.jsx";
+
+// Styles
+import "./Home.css"
 
 function Home() {
+    const [movies, setMovies] = useState([]);
+    const [moreMovies, setMoreMovies] = useState(false);
+    const [moviePage, setMoviePage] = useState(1);
+    const [totalMoviePages, setTotalMoviePages] = useState(0);
+
+    const [series, setSeries] = useState({});
+    const [moreSeries, setMoreSeries] = useState(false);
+    const [seriesPage, setSeriesPage] = useState(1);
+    const [totalSeriesPages, setTotalSeriesPages] = useState(0);
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         accept: 'application/json',
+    //         Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+    //     }
+    // };
+
 
 
     return (
@@ -10,8 +38,12 @@ function Home() {
                 <h1 className="home-titles">
                     Trending Movies
                 </h1>
-
+                <div className="loading-error-section">
+                    {loading && <h3 className="loading-message">Laden... </h3>}
+                    {error && <h3 className="error-message">Foutmelding: Er kan geen data opgehaald worden!</h3>}
+                </div>
             </div>
+
         </>
     );
 }

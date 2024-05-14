55799d6bf546faee41e87d7933769ff5ad632f11
Mark Mesander <mark_mesander@hotmail.com>
2024-05-14 23:57:28 +0200

    add general styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,9 @@ import React from "react";
 
 function Home() {
     return(
-        <h1>Home Page</h1>
+        <div className="page-outer-container">
+            <h1>Home Page</h1>
+        </div>
     );
 }
 

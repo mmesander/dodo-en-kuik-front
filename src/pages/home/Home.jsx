3eec7627e4f1a3240ece25918158e56835de0eac
Mark Mesander <mark_mesander@hotmail.com>
2024-05-15 00:14:35 +0200

    add button component and implement in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,17 @@
 import React from "react";
 
 function Home() {
-    return(
-        <div className="page-outer-container">
-            <h1>Home Page</h1>
-        </div>
+
+
+    return (
+        <>
+            <div className="page-outer-container">
+                <h1 className="home-titles">
+                    Trending Movies
+                </h1>
+
+            </div>
+        </>
     );
 }
 

f78df0e095eabc53607661963f05dfa7f66ec75c
Mark Mesander <mark_mesander@hotmail.com>
2024-06-03 17:49:59 +0200

    strip unused code from home and add TrendingMovies page

--- src/pages/trending/TrendingMovies.jsx
+++ src/pages/trending/TrendingMovies.jsx
@@ -1,4 +1,5 @@
 // Functions
+import {useNavigate, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "axios";
 
@@ -7,17 +8,19 @@ import Button from "../../components/button/Button.jsx";
 import MovieCard from "../../components/moviecard/MovieCard.jsx"
 
 // Styles
-import "./Home.css"
-import {useNavigate} from "react-router-dom";
+import "../home/Home.css"
 
-function Home() {
+function TrendingMovies() {
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const pageNumber = useParams().pageId;
+    const [page, setPage ] = useState(parseInt(pageNumber) || 1);
+    const [totalPages, setTotalPages] = useState(0);
 
     const [movies, setMovies] = useState([]);
     const [series, setSeries] = useState({});
-
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
 
     const options = {
         method: 'GET',
@@ -28,45 +31,52 @@ function Home() {
     };
 
     useEffect(() => {
-        async function fetchMovies() {
-            setLoading(true);
-            try {
-                const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/day?page=1`, options);
-                if (response.data) {
-                    setError(false);
-                }
-                setMovies(response.data.results);
+        if (page >= 1) {
+            void fetchMovies()
+            void fetchSeries()
+        }
+        updateUrl();
+    }, [page]);
 
-            } catch (e) {
-                setError(true);
-                console.error(e);
+    function updateUrl() {
+        const newUrl = `/trending-films/${page}`;
+        navigate(newUrl, {replace: true});
+    }
+
+    async function fetchMovies() {
+        setLoading(true);
+        try {
+            const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/day?page=${page}`, options);
+            if (response.data) {
+                setError(false);
             }
-            setLoading(false);
+            setMovies(response.data.results);
+            setTotalPages(response.data.total_pages);
+        } catch (e) {
+            setError(true);
+            console.error(e);
         }
+        setLoading(false);
+    }
 
-        async function fetchSeries() {
-            setLoading(true);
-            try {
-                const response = await axios.get(`https://api.themoviedb.org/3/trending/tv/day?page=1`, options);
-                if (response.data) {
-                    setError(false);
-                }
-                setSeries(response.data.results);
-
-            } catch (e) {
-                setError(true);
-                console.error(e);
+    async function fetchSeries() {
+        setLoading(true);
+        try {
+            const response = await axios.get(`https://api.themoviedb.org/3/trending/tv/day?page=1`, options);
+            if (response.data) {
+                setError(false);
             }
-            setLoading(false);
-        }
+            setSeries(response.data.results);
 
-        if (movies) {
-            void fetchMovies();
+        } catch (e) {
+            setError(true);
+            console.error(e);
         }
-        if (series) {
-            void fetchSeries();
-        }
-    }, []);
+        setLoading(false);
+    }
+
+
+
 
     return (
         <>
@@ -78,8 +88,24 @@ function Home() {
                     {loading && <h3 className="loading-message">Laden... </h3>}
                     {error && <h3 className="error-message">Foutmelding: Er kan geen data opgehaald worden!</h3>}
                 </div>
+                <div className="button-set-page-section">
+                    <Button
+                        type="button"
+                        clickHandler={() => setPage(page - 1)}
+                        disabled={page === 1}
+                    >
+                        Vorige
+                    </Button>
+                    <Button
+                        type="button"
+                        clickHandler={() => setPage(page + 1)}
+                        disabled={page === totalPages}
+                    >
+                        Volgende
+                    </Button>
+                </div>
                 <div className="home-inner-container">
-                    {movies && Object.keys(movies).length > 0 && movies.slice(0, 5).map((movie) => {
+                    {movies && Object.keys(movies).length > 0 && movies.map((movie) => {
                         return <MovieCard
                             key={movie.id}
                             title={movie.title}
@@ -93,10 +119,10 @@ function Home() {
                 </div>
                 <Button
                     type="button"
-                    name="inactive-home-results-button"
-                    clickHandler={() => navigate("/trending-films/1")}
+                    name="active-home-results-button"
+                    clickHandler={() => navigate("/")}
                 >
-                    Laat meer films zien
+                    Laat minder films zien
                 </Button>
                 <h1 className="home-titles">Trending Series</h1>
                 <div className="loading-error-section">
@@ -114,17 +140,21 @@ function Home() {
                             isMovie={false}
                         />
                     })}
+                    <Button
+                        type="button"
+                        name="inactive-home-results-button"
+                        clickHandler={() => navigate("/trending-series/1")}
+                    >
+                        Laat meer series zien
+                    </Button>
                 </div>
-                <Button
-                    type="button"
-                    name="inactive-home-results-button"
-                    clickHandler={() => navigate("/trending-series/1")}
-                >
-                   Laat meer series zien
-                </Button>
             </div>
+
+
         </>
-    );
+    )
+
+
 }
 
-export default Home;
+export default TrendingMovies;
---
+++ src/pages/trending/TrendingSeries.jsx
@@ -0,0 +1,3 @@
+function TrendingSeries() {}
+
+export default TrendingSeries;

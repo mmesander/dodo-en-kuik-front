c872caa3ae7ce0ce00f0087726aa4e05a2ef7277
Mark Mesander <mark_mesander@hotmail.com>
2024-06-03 13:53:12 +0200

    add helper createFilterStrings

---
+++ src/helpers/createFilterString.jsx
@@ -0,0 +1,25 @@
+function createFilterStrings(isMovie, genresList, minRating, maxRating) {
+    const genresString = "&with_genres=";
+    const minRatingString = "&vote_average.gte=";
+    const maxRatingString = "&vote_average.lte=";
+    const type = isMovie ? 'movieGenres' : 'seriesGenres';
+
+    let genresUrlString;
+    let ratingUrlString;
+
+    if (genresList[type].length === 0) {
+        genresUrlString = "";
+    } else if (genresList[type].length === 1) {
+        genresUrlString = genresString + genresList[type][0];
+    } else {
+        const numbersToString = genresList[type].map((id) => id.toString());
+        const joinedNumbers = numbersToString.join('%2C');
+        genresUrlString = genresString + joinedNumbers;
+    }
+
+    ratingUrlString = minRatingString + minRating + maxRatingString + maxRating;
+
+    return [genresUrlString, ratingUrlString];
+}
+
+export default createFilterStrings;

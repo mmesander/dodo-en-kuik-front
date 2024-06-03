880867216f9ee507b935c6228b628813f7fd21f2
Mark Mesander <mark_mesander@hotmail.com>
2024-06-03 18:58:40 +0200

    add styling for filter search

--- src/pages/search/SpecificSearch.jsx
+++ src/pages/search/SpecificSearch.jsx
@@ -1,3 +1,15 @@
+// Functions
+import {useEffect, useState} from "react";
+import {useLocation, useNavigate, useParams} from "react-router-dom";
+import axios from "axios";
+
+// Components
+import Button from "../../components/button/Button";
+import MovieCard from "../../components/moviecard/MovieCard";
+
+// Styles
+import "./Search.css";
+
 function SpecificSearch() {}
 
 export default SpecificSearch;

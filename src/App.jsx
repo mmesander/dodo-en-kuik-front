b7b587a2d959dc88002a278afbcb1a58b92f2763
Mark Mesander <mark_mesander@hotmail.com>
2024-09-24 14:30:46 +0200

    add FilterSearch page and implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,20 @@ import Footer from "./components/footer/Footer.jsx";
 
 // Pages
 import Home from "./pages/home/Home.jsx";
+import TrendingMovies from "./pages/trending/TrendingMovies.jsx";
+import TrendingSeries from "./pages/trending/TrendingSeries.jsx";
+
 import Search from "./pages/search/Search.jsx";
+import SpecificSearch from "./pages/search/SpecificSearch.jsx";
+import FilterSearch from "./pages/search/FilterSearch.jsx";
+
 import Suggestion from "./pages/suggestion/Suggestion.jsx";
+
 import Lists from "./pages/lists/Lists.jsx";
+import ListsFavorites from "./pages/lists/ListsFavorites.jsx";
+import ListsWatched from "./pages/lists/ListsWatched.jsx";
+import ListsWatchlist from "./pages/lists/ListsWatchlist.jsx";
+
 import Profile from "./pages/profile/Profile.jsx";
 
 import SignUp from "./pages/signup/SignUp.jsx";
@@ -22,16 +33,8 @@ import SignIn from "./pages/signin/SignIn.jsx";
 import MovieDetails from "./pages/details/MovieDetails.jsx";
 import SeriesDetails from "./pages/details/SeriesDetails.jsx";
 
-import ListsFavorites from "./pages/lists/ListsFavorites.jsx";
-import ListsWatched from "./pages/lists/ListsWatched.jsx";
-import ListsWatchlist from "./pages/lists/ListsWatchlist.jsx";
-
-import TrendingMovies from "./pages/trending/TrendingMovies.jsx";
-import TrendingSeries from "./pages/trending/TrendingSeries.jsx";
-
 // Styles
 import './App.css'
-import SpecificSearch from "./pages/search/SpecificSearch.jsx";
 
 function App() {
     const {isAuth} = useContext(AuthContext);
@@ -45,6 +48,7 @@ function App() {
                 <Route path="/trending-series/:pageId" element={<TrendingSeries/>}/>
                 <Route path="/zoeken/overzicht/:filterId" element={<Search/>}/>
                 <Route path="/zoeken/specifiek/:searchId" element={<SpecificSearch/>}/>
+                <Route path="/zoeken/filter/:filterId" element={<FilterSearch/>}/>
                 <Route path="/suggestie" element={<Suggestion/>}/>
                 <Route path="/lijsten" element={<Lists/>}/>
                 <Route path="/lijsten/favorieten" element={<ListsFavorites/>}/>

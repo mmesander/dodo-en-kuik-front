11a25850309cbca37563ffdaf42a03a870d0bbfe
Mark Mesander <mark_mesander@hotmail.com>
2024-10-29 15:12:18 +0100

    add mood containers for several moods and trim code

--- src/pages/suggestion/Suggestion.jsx
+++ src/pages/suggestion/Suggestion.jsx
@@ -1,8 +1,10 @@
 // Functions
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 // Components
 import MoodContainer from "../../components/moodcontainer/MoodContainer.jsx";
+import Button from "../../components/button/Button.jsx";
 
 // Assets
 import comedy from "../../assets/images/mood-laugh.jpg"
@@ -17,27 +19,54 @@ import "./Suggestion.css"
 function Suggestion() {
     const navigate = useNavigate();
 
-    function clickHandler(mood) {
-        const url = `/suggestie/${mood}/1`;
+    // State om te bepalen of er op films of series geselecteerd gaat worden
+    const [isMovie, setIsMovie] = useState(true);
+
+    const moods = [
+        {mood: "Van de bank te rollen van het lachen", genre: "komische", image: comedy},
+        {mood: "Op het puntje van je stoel te zitten", genre: "spannende", image: adventure},
+        {mood: "De stuipen op het lijf gejaagd te worden", genre: "enge", image: horror},
+        {mood: "In een andere wereld te belanden", genre: "fantasierijke", image: fantasy},
+        {mood: "Met een doos tissues op de bank te zitten", genre: "dramatische", image: drama},
+    ];
+
+    function clickHandler(genre) {
+        const type = isMovie ? "films" : "series";
+        const url = `/suggestie/${genre}-${type}/1`
         navigate(url);
     }
 
-
-
-    return(
+    return (
         <div className="page-outer-container">
             <h1 className="suggestion-title">Heb jij zin om: </h1>
+            <div className="suggestion-page-button-container">
+                <Button
+                    type="button"
+                    clickHandler={() => setIsMovie(true)}
+                    disabled={isMovie}
+                >
+                    Films
+                </Button>
+                <Button
+                    type="button"
+                    clickHandler={() => setIsMovie(false)}
+                    disabled={!isMovie}
+                >
+                    Series
+                </Button>
+            </div>
             <div className="suggestion-page-mood-container">
-                <MoodContainer
-                    mood="van de bank te rollen van het lachen"
-                    image={comedy}
-                    imageDescription={"mood image for comedy movies"}
-                    // onClick={() => clickHandler()}
-                />
+                {moods && moods.map(({mood, genre, image}) => (
+                    <MoodContainer
+                        key={genre}
+                        mood={mood}
+                        image={image}
+                        imageDescription={`Mood image for ${isMovie ? "movies" : "series"}`}
+                        onClick={() => clickHandler(genre)}
+                    />
+                ))}
             </div>
         </div>
-
-
     );
 }
 

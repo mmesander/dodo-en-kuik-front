853a4778c878e79db405c4219a8370d70e6baced
Mark Mesander <mark_mesander@hotmail.com>
2024-10-29 11:01:00 +0100

    add MoodContainer component

---
+++ src/components/moodcontainer/MoodContainer.jsx
@@ -0,0 +1,31 @@
+// Functions
+import PropTypes from "prop-types";
+
+// Styles
+import "./MoodContainer.css";
+
+MoodContainer.propTypes = {
+    mood: PropTypes.string,
+    image: PropTypes.any,
+    imageDescription: PropTypes.string,
+    clickHandler: PropTypes.func,
+}
+
+function MoodContainer({mood, image, imageDescription, clickHandler}) {
+    return(
+        <>
+            <button
+                type="button"
+                className="mood-button"
+                onClick={clickHandler}
+            >
+                <div className="mood-inner-conainter">
+                    <img src={image} alt={imageDescription}/>
+                    <h4 className="mood-description">{mood}</h4>
+                </div>
+            </button>
+        </>
+    );
+}
+
+export default MoodContainer;

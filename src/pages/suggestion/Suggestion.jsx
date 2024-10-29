3351f768d03110593d5f889158a5e7744ec275bb
Mark Mesander <mark_mesander@hotmail.com>
2024-10-29 10:52:58 +0100

    setup base structure of suggestion page

--- src/pages/suggestion/Suggestion.jsx
+++ src/pages/suggestion/Suggestion.jsx
@@ -1,7 +1,19 @@
 // Functions
-import React from "react";
+import {useNavigate} from "react-router-dom";
+
+// Components
+
+// Assets
+
+
+// Styles
+import "./Suggestion.css"
 
 function Suggestion() {
+    const navigate = useNavigate();
+
+
+
     return(
         <h1>Suggestion Page</h1>
     );

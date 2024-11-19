36aedb5a22fd0965bdf86c349a0129adec6ed829
Mark Mesander <mark_mesander@hotmail.com>
2024-11-19 14:29:48 +0100

    add basic functions and parameters

--- src/pages/suggestion/SpecificSuggestion.jsx
+++ src/pages/suggestion/SpecificSuggestion.jsx
@@ -4,6 +4,25 @@
 
 // Styles
 
-function SpecificSuggestion() {}
+import {useLocation, useNavigate, useParams} from "react-router-dom";
+import {useEffect} from "react";
+
+function SpecificSuggestion() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const pageNumber = useParams().pageId;
+    const chosenSuggestion = useParams().moodId.toString();
+
+    const [genre, type] = chosenSuggestion.split('-');
+
+    useEffect(() => {
+        console.log(genre)
+        console.log(type)
+    }, []);
+
+
+
+}
 
 export default SpecificSuggestion;
